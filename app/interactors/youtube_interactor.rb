bea5253a692244ca119965e93ba1a7d6829ff555
Richard <wei.richard.s@gmail.com>
2019-12-20 16:26:12 -0800

    add video accordion

--- app/interactors/youtube_interactor.rb
+++ app/interactors/youtube_interactor.rb
@@ -1,6 +1,7 @@
 class YoutubeInteractor
 
-  KEY = 
+  # in a real world scenario I'd probably put this in a ENV variable. Wouldn't want others to hijack our API Key
+  KEY = "AIzaSyBAln3nc-CSnBuJuOK_l1su7gxgpIdX5xk" 
   BASE_URI = 'https://www.googleapis.com/youtube/v3/'.freeze
 
   def initialize(query = '', ids = [])
